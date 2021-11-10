ed24425c5c0ab0b6fff4ea201e2d5f6064fddfc8
aayush <aayush.goyal54@gmail.com>
2021-11-10 13:16:30 +0530

    update

--- eval.py
+++ eval.py
@@ -7,7 +7,7 @@ from torchvision import transforms, models
 import os
 from tqdm import tqdm
 
-labels = ['background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+
 model = models.segmentation.deeplabv3_resnet101(pretrained=True).eval()
 cmap = plt.cm.get_cmap('tab20c')
 colors = (cmap(np.arange(cmap.N)) * 255).astype(np.int)[:, :3].tolist()
@@ -16,17 +16,6 @@ np.random.shuffle(colors)
 colors.insert(0, [0, 0, 0]) # background color must be black
 colors = np.array(colors, dtype=np.uint8)
 
-palette_map = np.empty((10, 0, 3), dtype=np.uint8)
-legend = []
-
-for i in range(21):
-    legend.append(mpatches.Patch(color=np.array(colors[i]) / 255., label='%d: %s' % (i, labels[i])))
-    c = np.full((10, 10, 3), colors[i], dtype=np.uint8)
-    palette_map = np.concatenate([palette_map, c], axis=1)
-
-# plt.figure(figsize=(20, 2))
-# plt.legend(handles=legend)
-# plt.imshow(palette_map)
 if torch.cuda.is_available():
         print("Working on CUDA")
 def segment(net, img):
@@ -69,6 +58,10 @@ def eval(img,gt):
     segment_map, pred = segment(model, img)
     gt[gt>0] = 1
     pred[pred>0] = 1
+    print("pred")
+    plt.imshow(pred)
+    print("gt")
+    plt.imshow(gt)
     return mIOU(pred,gt)
 
 
@@ -90,7 +83,4 @@ for i in os.listdir(dataset):
     
 print(categorical_miou)
 
-# fig, axes = plt.subplots(1, 2, figsize=(20, 10))
-# axes[0].imshow(img)
-# axes[1].imshow(segment_map)
 
